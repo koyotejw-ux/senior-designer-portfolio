16d9fe28483f0273ad89c5dff9b6cbcefebe8b88
koyotejw-ux <koyotejw-ux@users.noreply.github.com>
2026-06-25 16:32:40 +0000

    deploy: d9b8e596f9535b219c4e90771f985c4251e1cf41

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "273380787"
+    serviceWorkerVersion: "1912990155"
   }
 });
