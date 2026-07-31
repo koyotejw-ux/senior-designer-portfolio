3cb2a259b2e2fed16b1edd6c2682a224b6d637f6
WordApp Developer <developer@wordapp.com>
2026-07-31 12:51:00 +0900

    fix: PDF 3페이지 포트폴리오 사이트 안내 문구 제거

--- lib/features/admin/presentation/widgets/project_print_dialog.dart
+++ lib/features/admin/presentation/widgets/project_print_dialog.dart
@@ -364,37 +364,6 @@ class _ProjectPrintDialogState extends State<ProjectPrintDialog> {
             );
           }
 
-          // 포트폴리오 사이트 안내 (가장 하단)
-          final double guideY = firstRowY + totalListHeight + marginBelowList;
-
-          textOverlays.add(
-            pw.Positioned(
-              left: numX,
-              top: guideY,
-              child: pw.Column(
-                crossAxisAlignment: pw.CrossAxisAlignment.start,
-                children: [
-                  pw.Text(
-                    '더 많은 포트폴리오와 상세 내용은 아래 사이트 주소에서 확인하실 수 있습니다.',
-                    style: pw.TextStyle(
-                      font: pretendardRegular,
-                      fontSize: 20 * scaleRatio,
-                      color: const PdfColor(0.78, 0.80, 0.84),
-                    ),
-                  ),
-                  pw.SizedBox(height: 12 * scaleRatio),
-                  pw.Text(
-                    'https://koyotejw-ux.github.io/senior-designer-portfolio',
-                    style: pw.TextStyle(
-                      font: pretendardBold,
-                      fontSize: 24 * scaleRatio,
-                      color: PdfColors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
         }
 
         cumY += pageH;
